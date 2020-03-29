3f8c792467be84f7ca314322d79d533b1a9bf498
Martin Nieves <mnieves@createthrive.com>
2020-03-29 16:42:37 -0300

    Stay alive

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { startBot } from './bot/start-bot';
 
 startBot();
+
+//Stay awake on Glitch.com
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  console.log('server pinged');
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
