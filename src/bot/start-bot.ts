2c7b0edf2690e7cedeb30db2f549b8f3f5136527
Martin Nieves <mnieves@createthrive.com>
2020-03-31 18:16:34 -0300

    Change datasource

--- src/bot/start-bot.ts
+++ src/bot/start-bot.ts
@@ -15,42 +15,49 @@ export function startBot() {
   const random = <T>(items: T[]) => items[Math.floor(Math.random() * items.length)];
   const isBotId = (id: string) => id === configService.botId;
 
-  bot.on('message', msg => {
+  bot.on('message', async msg => {
 
     if (msg.content === '-covid echo')
       msg.channel.send(formatMsg('echo', ''));
 
-    if (msg.mentions.members.some(x => isBotId(x.id)))
+    if (msg.mentions.members.some(x => isBotId(x.id))) {
       msg.reply(
         random([
-          'hi',
-          'how are you?',
-          'hello',
           'psst, wanna see some magic? Try typing `-covid UY`',
+          'I can show you the latest covid info per country, try this: `-covid IT`',
           'I got some bat soup, want to share?'
         ])
       );
+    }
+    const capitalizeFirst = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
+    const capitalizeFirstMultiple = (string: string) => string.split(' ').map(x => capitalizeFirst(x)).join(' ');
 
     if (msg.content !== '-covid echo' && msg.content.includes('-covid') && !isBotId(msg.member.id)) {
       msg.react('👍');
-      const countryCode = msg.content.split(' ').pop().toUpperCase();
-      coronaService.getCasesByCountry(countryCode).then(resp => {
-        const confirmed = resp.data.latest.confirmed;
-        const deaths = resp.data.latest.deaths;
-        const country = resp.data.locations[0].country;
+      try {
+        let countryCode = msg.content.split(' ').slice(1).join(' ').toLowerCase();
+        if (countryCode.length == 2) countryCode = countryCode.toUpperCase();
+        const resp = await coronaService.getAllCases();
+        const country = capitalizeFirstMultiple(countryCode);
+        console.log(country);
+        const countryData = resp.data[country];
+        const confirmed = countryData[countryData?.length - 1].confirmed;
+        const recovered = countryData[countryData?.length - 1].recovered;
+        const deaths = countryData[countryData?.length - 1].deaths; 
         msg.channel.send(
           [
             `Displaying Covid-19 cases for ${country}`,
             '\n',
             formatMsg(`Confirmed: ${confirmed}`),
+            formatMsg(`Recovered: ${recovered}`),
             formatMsg(`Deaths: ${deaths}`),
           ].join('')
         );
-      }, _err => {
+      } catch (_err) {
+        console.log(_err);
         msg.channel.send(`Whoops, country code was not found.`);
-      });
+      };
     }
   });
   bot.login(configService.botToken);
-
 }
